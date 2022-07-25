af6eda25107518459a515bff9dec7142e7753eb5
Bagus Kurnia <bagus.kurnia@sirclo.com>
2022-07-25 12:39:21 +0700

    Add cart layout

--- src/pages/cart/cart.tsx
+++ src/pages/cart/cart.tsx
@@ -2,17 +2,53 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 
 import Breadcrumb from "../../components/uis/breadcrumb/breadcrumb";
+import CardCart from "../../components/uis/card-cart/card-cart";
+import "./cart.scss";
 
 const Cart: React.FC = () => {
   return (
     <div>
       <Breadcrumb path={useLocation().pathname} />
-      <div className="py-6 px-4 lg:px-[55px] max-w-[1440px] lg:mx-auto lg:pt-8">
-        <div>
-          <h1 className="text-grey-700 font-bold text-2xl">Keranjang</h1>
-          <p className="text-grey-700 font-normal text-sm">3 Produk</p>
+      <div className="py-6 px-4 lg:px-[55px] max-w-[1440px] lg:mx-auto lg:pt-8 sm:flex sm:flex-row sm:justify-between">
+        <div className="grow sm:pr-[30px]">
+          <div className="sm:flex sm:justify-between">
+            <h1 className="text-grey-700 font-bold text-2xl">Keranjang</h1>
+            <p className="text-grey-700 font-normal text-sm">3 Produk</p>
+          </div>
+          <div className="border border-grey-300 mt-4 rounded p-4">
+            <CardCart />
+            <CardCart />
+            <CardCart />
+          </div>
         </div>
-        <div className="border border-grey-300"></div>
+        <div>
+          <div className="mt-[21px] border border-grey-300 rounded p-4 bg-grey-050 sm:mt-0">
+            <div className="flex items-center justify-between">
+              <p className="font-normal text-grey-700 text-xs">Harga barang</p>
+              <p className="font-normal text-grey-700 text-xs">USD 80</p>
+            </div>
+            <div className="flex items-center justify-between pt-2">
+              <p className="font-normal text-grey-700 text-xs">
+                Pengiriman (ANTERAJA - REG)
+              </p>
+              <p className="font-normal text-grey-700 text-xs">USD 5</p>
+            </div>
+            <div className="flex items-center justify-between pt-2">
+              <p className="font-normal text-grey-700 text-xs">
+                Biaya pembayaran
+              </p>
+              <p className="font-normal text-grey-700 text-xs">USD 0</p>
+            </div>
+            <div className="divider border-dashed	border-[0.5px] border-grey-300 my-3"></div>
+            <div className="flex items-center justify-between pt-2">
+              <p className="font-bold text-grey-700 text-xs">Total Tagihan</p>
+              <p className="font-bold text-grey-700 text-xs">USD 85</p>
+            </div>
+          </div>
+          <button className="bg-blue-300 mt-[21px] text-white	rounded h-12 w-full flex items-center justify-center font-bold text-base">
+            Beli Sekarang
+          </button>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,15 @@ module.exports = {
     extend: {},
     colors: {
       "grey-0": "#fff",
+      "grey-050": "#FAFBFC",
       "grey-200": "#EFF2F5",
       "grey-300": "#D0D9E0",
       "grey-400": "#7590A3",
       "grey-700": "#52697A",
       "grey-800": "#3D4F5C",
       "grey-900": "#1E272E",
+      "blue-300": "#269CD9",
+      white: "#fff",
     },
     screens: {
       sm: "600px",
